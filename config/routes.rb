2628c60ab017c32cd9bfb75a9f1a1ca98b1cd7a3
Nancy Do <s3510210@student.rmit.edu.au>
2017-04-06 06:37:25 +0000

    Changed root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'static_pages/category'
 
   get 'static_pages/locations'
 
   get 'static_pages/login'
-
-  root 'application#hello'
 
   get 'static_pages/login'
   
