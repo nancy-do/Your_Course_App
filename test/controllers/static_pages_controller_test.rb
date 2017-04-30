6dfb773da814c02af0bed73e8739d32ee55092db
Nancy Do <s3510210@student.rmit.edu.au>
2017-04-30 14:41:11 +0000

    Layout default

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,17 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get category" do
-    get static_pages_category_url
+    get category_path
     assert_response :success
   end
 
   test "should get locations" do
-    get static_pages_locations_url
-    assert_response :success
-  end
-
-  test "should get login" do
-    get static_pages_login_url
+    get locations_path
     assert_response :success
   end
 
