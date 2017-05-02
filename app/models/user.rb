ac85f410eafe2def9ee1c1943371c49829ce0e4b
Nabila Herzegovina <s3463558@student.rmit.edu.au>
2017-05-02 08:37:39 +0000

    Validate uniqueness of Email and convert them to lower case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase } 
   validates :name, presence: true, length: {minimum: 4}
   VALID_EMAIL_REGEX = /([a-z+]+)+\.+([a-z+]+)+@rmit.edu.au/i
   # VALID_EMAIL_REGEX = /\d/
-  validates :email, presence: true, length: {minimum: 4}, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, length: {minimum: 4}, format: {with: VALID_EMAIL_REGEX, message: "Email Registration only open to RMIT staff" }, uniqueness: { case_sensitive: false }
 end
