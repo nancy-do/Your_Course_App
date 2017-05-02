86dd87e20fceb85e816b7e6817d45529f4388688
Nabila Herzegovina <s3463558@student.rmit.edu.au>
2017-05-02 08:26:25 +0000

    Add email validation for RMIT Staff

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "example.user@rmit.edu.au")
   end
 
   test "should be valid" do
@@ -28,6 +28,15 @@ class UserTest < ActiveSupport::TestCase
   test "email is too short" do
     @user.email = "aaa"
     assert_not @user.valid?
+  end
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com user.user@gmail.com user@rmit.edu.au]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
   
 end
