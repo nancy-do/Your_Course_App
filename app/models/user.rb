e25d61227ef3f2fb90fbcef74c86379cf553c286
Nabila Herzegovina <s3463558@student.rmit.edu.au>
2017-05-02 08:26:25 +0000

    Add email validation for RMIT Staff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {minimum: 4}
-  validates :email, presence: true, length: {minimum: 4}
+  VALID_EMAIL_REGEX = /([a-z+]+)+\.+([a-z+]+)+@rmit.edu.au/i
+  # VALID_EMAIL_REGEX = /\d/
+  validates :email, presence: true, length: {minimum: 4}, format: {with: VALID_EMAIL_REGEX}
 end
