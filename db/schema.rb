d5fbb5ccbedd366c4497d92c5b92c38561bd9295
Nancy Do <s3510210@student.rmit.edu.au>
2017-05-02 00:36:35 +0000

    Started courses implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501054106) do
+ActiveRecord::Schema.define(version: 20170501104114) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "prerequisite"
+    t.string   "description"
+    t.string   "category"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "locations", force: :cascade do |t|
---
+++ test/controllers/courses_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CoursesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get create_course_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    get courses_path
+    assert_response :success
+  end
+  
+  test "should get create" do
+    get create_course_path
+    assert_response :success
+  end
+
+end
