9f824449d61b163892d4ec2990f7da40a0dabc0d
Nabila Herzegovina <s3463558@student.rmit.edu.au>
2017-05-03 23:41:40 +0000

    Add validation when crating courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+    validates :name , presence: true, length: {minimum: 10}
+    validates :prerequisite, presence: true, length: {minimum: 10}
+    validates :description, presence: true, length: {minimum: 30}
 end
