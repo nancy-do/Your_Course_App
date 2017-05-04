ceb36915db4b258aadc1628742e3e5565fc7ae6b
Nabila Herzegovina <s3463558@student.rmit.edu.au>
2017-05-04 07:12:12 +0000

    Validate locations digit

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,8 @@
 class Location < ApplicationRecord
     
-    validates :building, presence: true, :uniqueness => {:scope => [:level, :room]}
-    validates :level, presence: true
-    validates :room, presence: true
+    validates :building, presence: true, :uniqueness => {:scope => [:level, :room]}, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 1000 , only_interger: true}
+    validates :level, presence: true, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 30 , only_interger: true}
+    validates :room, presence: true, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 100, only_interger: true}
     
     def locationName
         "#{building}.#{level}.#{room}"
