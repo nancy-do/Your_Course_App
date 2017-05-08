aba424cc35595ec4ccde6c26c36e2714a7169c55
Nabila Herzegovina <s3463558@student.rmit.edu.au>
2017-05-08 03:10:13 +0000

    Modify seed.rb to require having locationsIDs and CategoryIDs

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,11 @@ Category.create!(name:"Full Stack Dev")
 Category.create!(name:"Application Development")
 
 Course.create!(name:"Programming 1", prerequisite: "Professional Computing Practise", 
-                description:"Example Description",user_id:1)
+                description:"Example Description",user_id:1, location_ids: [1], category_ids: [2])
 Course.create!(name:"Professional Computing Practise", prerequisite: "Programming 1", 
-                description:"Example Description",user_id:1)
+                description:"Example Description",user_id:1, location_ids: [2], category_ids: [2])
 Course.create!(name:"Rapid Application Development", prerequisite: "Programming 1", 
-                description:"Example Description",user_id:2)
+                description:"Example Description",user_id:2, location_ids: [3], category_ids: [1])
 
 Course.first.locations << Location.find(1)
 Course.second.locations << Location.find(2)
