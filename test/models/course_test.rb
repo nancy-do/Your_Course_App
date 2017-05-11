6779a697a3f0d6fa928ff9dc76c3c48a1da81203
Nancy Do <s3510210@student.rmit.edu.au>
2017-05-11 06:44:25 +0000

    Course

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,31 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @category = Category.new(name: "Web Servers and Web Technology")
+    @location = Location.new(building: 10, level: 10, room:30)
+    @user = User.new(name: "Example User", email: "example.user@rmit.edu.au", password:"foOBar123!@#", password_confirmation:"foOBar123!@#")
+    @course = Course.new(name:"Example Examplee", prerequisite: "Example exampskhj", description:"Example Description",
+                          user_id: 1, location_ids: [@location.id], category_ids: [@category.id])
+  end 
+  
+  test "should be valid" do
+    assert @course.valid?
+  end
+  
+  # test "name should be present" do
+  #   @course.name = "     "
+  #   assert_not @course.valid?
+  # end
+  
+  # test "Prerequisite  should be present" do
+  #   @course.prerequisite = "     "
+  #   assert_not @course.prerequisite?
+  # end
+  
+  # test "description  should be present" do
+  #   @course.description = "     "
+  #   assert_not @course.description?
+  # end
 end
