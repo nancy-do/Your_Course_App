48851716310075ba067c876a8b806751377730fe
Nabila Herzegovina <s3463558@student.rmit.edu.au>
2017-05-11 06:49:11 +0000

    Finish test for Message model

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @message = Message.new(name: "Example Name", message: "Hello, this is a sample message!", email:"example.example@gmail.com")
+  end
+  
+  test "should be valid" do
+    assert @message.valid?
+  end
+  
+  test "name should be present" do
+    @message.name = " "
+    assert_not @message.name?
+  end
+  
+  test "email should be present" do
+    @message.email = " "
+    assert_not @message.email?
+  end
+  
+  test "message should be present" do
+    @message.message = " "
+    assert_not @message.message?
+  end
+  
+  test "name should be longer than 4 chars" do
+    @message.name = "abc"
+    assert_not @message.valid?
+  end
+  
+  test "message is too long" do
+    @message.message = "a" * 300
+    assert_not @message.valid?
+  end
+  
+  test "email is not valid" do
+    @message.email = "abc.asdcds.com@"
+    assert_not @message.valid?
+  end
+  
 end
