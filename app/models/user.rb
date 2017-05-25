34c7b34ea96e9abba5c7ab27c8faf9547b523292
Nancy Do <s3510210@student.rmit.edu.au>
2017-05-25 08:18:29 +0000

    Created admin user with appropriate validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,42 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase } 
   validates :name, presence: true, length: {minimum: 4}
   VALID_EMAIL_REGEX = /([a-z+]+)+\.+([a-z+]+)+@rmit.edu.au/i
-  # VALID_EMAIL_REGEX = /\d/
-  validates :email, presence: true, length: {minimum: 4}, format: {with: VALID_EMAIL_REGEX, message: "Email Registration only open to RMIT staff" }, uniqueness: { case_sensitive: false }
+  #---------------------------Checking email
+  validates :email, 
+    format: {with: /\b(admin)\b/},
+    uniqueness: true,
+    :if => Proc.new {|c| c.admin == true}
+
+  validates :email, 
+    presence: true, 
+    length: {minimum: 4}, 
+    format: {with: VALID_EMAIL_REGEX, message: "Email Registration only open to RMIT staff" }, 
+    uniqueness: { case_sensitive: false }, 
+    :unless => Proc.new {|c| c.admin == true}
+
+  #------------------- Checking email 
+  #validates :email, presence: true, length: {minimum: 4}, format: {with: VALID_EMAIL_REGEX, message: "Email Registration only open to RMIT staff" }, uniqueness: { case_sensitive: false }
   has_secure_password
   VALID_PASSWORD_REGEX = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,}/
-  validates :password, presence: true, length: {minimum: 8}, format: {with: VALID_PASSWORD_REGEX, message: "Password must at least contain an lower case, an upper case, a digit, a special character and contains 8+ characters"}
+  #validates :password, presence: true, length: {minimum: 8}, format: {with: VALID_PASSWORD_REGEX, message: "Password must at least contain an lower case, an upper case, a digit, a special character and contains 8+ characters"}
+  #---------------------------Checking password 
+  validates :password, 
+    format: {with: /\b(password)\b/},
+    :if => Proc.new {|c| c.admin == true}
 
+  validates :password, 
+    presence: true, 
+    length: {minimum: 8}, 
+    format: {with: VALID_PASSWORD_REGEX, message: "Password must at least contain an lower case, an upper case, a digit, a special character and contains 8+ characters"},
+    :unless => Proc.new {|c| c.admin == true}
+  #---------------------------Checking password 
+  
+  # with_options if: (:admin == true) do |admin|
+  #   PASSWORD_REGEX = /\b(password)\b/
+  #   EMAIL_REGEX = /\b(admin)\b/
+  #   admin.validates :password, format: {with: PASSWORD_REGEX}
+  #   admin.validates :email, format: {with: EMAIL_REGEX}
+  # end 
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -17,4 +47,5 @@ class User < ApplicationRecord
   has_many :courses
   has_many :rates
   has_many :ratings, through: :rates
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508135114) do
+ActiveRecord::Schema.define(version: 20170519101812) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -79,9 +79,10 @@ ActiveRecord::Schema.define(version: 20170508135114) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
